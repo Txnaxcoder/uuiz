ab1b54eec3c639b7ce340e54de5a35c57225bc22
Txnax <tanmaychandra10@gmail.com>
2022-10-31 16:31:59 +0530

    first commit

---
+++ public/ques.js
@@ -0,0 +1,193 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Most famous sweet of Karnataka?",
+    answer: "Mysore pak",
+    options: [
+      "obbattu",
+      "jamoon",
+      "Mysore pak",
+      "kai metai"
+    ]
+  },
+    {
+    numb: 2,
+    question: "First kannada dynasty of Karnataka?",
+    answer: "Kadamba",
+    options: [
+      "Kadamba",
+      "Chola",
+      "Chalukya",
+      "Mughal"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Classic dish of Karnataka?",
+    answer: "Bisi bele bath",
+    options: [
+      "Mosaranna",
+      "Puliogare",
+      "Vangi bath",
+      "Bisi bele bath"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Unique dance drama form of Karnataka?",
+    answer: "Yakshagana",
+    options: [
+      "Katakali",
+      "Kuchupudi",
+      "Yakshagana",
+      "Bharatanatya"
+    ]
+  },
+    {
+    numb: 5,
+    question: "When was Mysore state established?",
+    answer: "1st,November 1956",
+    options: [
+      "11th, Febuary 1960",
+      "1st,November 1956",
+      "3rd, december 1952",
+      "1st, Janauary 1950"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Total number of Districts in Karnataka?",
+    answer: "31",
+    options: [
+      "35",
+      "40",
+      "31",
+      "37"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Tagline of Karnataka tourism",
+    answer: "One state many worlds",
+    options: [
+      "Blisful Karnataka",
+      "One state many worlds",
+      "Incridible Karnataka",
+      "Gods own country"
+    ]
+  },
+  {
+    numb: 8,
+    question: "State flower of Karnataka",
+    answer: "Lotus",
+    options: [
+      "Sunflower",
+      "Sampige",
+      "Jasmine",
+      "Lotus"
+    ]
+  },
+  {
+    numb: 9,
+    question: "State tree of Karnataka",
+    answer: "Sandal wood",
+    options: [
+      "Teak",
+      "Sandal wood",
+      "Rose wood",
+      "Rubber wood"
+    ]
+  },
+  {
+    numb: 10,
+    question: "The first city in asia to light up the streets on electricity",
+    answer: "Bengaluru",
+    options: [
+      "Kolar",
+      "Bengaluru",
+      "Mysore",
+      "Tumkur"
+    ]
+  },
+  {
+    numb: 11,
+    question: "The state emblem of Karnataka?",
+    answer: "Gandabarunda",
+    options: [
+      "Gandabarunda",
+      "Garuda",
+      "Rose ringed Parakeet",
+      "Chakra"
+    ]
+  },
+  {
+    numb: 12,
+    question: "The state animal of Karnataka?",
+    answer: "Elephants",
+    options: [
+      "Tiger",
+      "Deer",
+      "Elephants",
+      "Lion"
+    ]
+  },
+  {
+    numb: 13,
+    question: "Rebelious queen of Karnataka?",
+    answer: "Kitturu Rani Channamma",
+    options: [
+      "Keladia Channamma",
+      "Rani Abbakka",
+      "Rani lakshimi bhai",
+      "Kitturu Rani Channamma"
+    ]
+  },
+  {
+    numb: 14,
+    question: "Worlds Biggest monolithic statue in Karnataka",
+    answer: "Gommateshwara",
+    options: [
+      "Gommateshwara",
+      "Gautham buddha",
+      "Staue of Liberty",
+      "Statue of unity"
+    ]
+  },
+  {
+    numb: 15,
+    question: "Highest peak of Karnataka?",
+    answer: "Mullayana giri",
+    options: [
+      "Nandhi hills",
+      "Mullayana giri",
+      "Brahmagiri hills",
+      "Kemmangundi"
+    ]
+  },
+  {
+    numb: 16,
+    question: "Karnataka is the largest _____ exxporter in the country",
+    answer: "Coffee",
+    options: [
+      "Coffee",
+      "Sandal wood",
+      "Spices",
+      "Sugar"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
---
+++ server.js
@@ -0,0 +1,33 @@
+const firebase = require("firebase");
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAkryPummY3a_Qi-YR4dV-VGUV8EZvJjFw",
+    authDomain: "quiz-8558b.firebaseapp.com",
+    databaseURL: "https://quiz-8558b-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "quiz-8558b",
+    storageBucket: "quiz-8558b.appspot.com",
+    messagingSenderId: "803148155342",
+    appId: "1:803148155342:web:7a14a22b97dd588f8c839f",
+    measurementId: "G-0GW1MMVKSR"
+  };
+  
+  firebase.initializeApp(firebaseConfig)
+  const db = firebase.database();
+  
+
+console.log("Server is running")
+const express = require("express")
+const app = express();
+app.listen(3000, () => console.log("listening at 3000"))
+app.use(express.static(__dirname + '/public'));
+app.use(express.json({limit: '1mb'}));
+var score;
+app.post('/api', (request, response) => {
+ console.log(request.body.person)
+ score = (request.body.userScore)
+ database.insert({personscore:score})
+ db.ref(request.body.person).set(score)
+});
+
